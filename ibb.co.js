56b694f1009ea48c80a6dc7ce79247b202d01bba
bga <bga.email@gmail.com>
2018-11-21 02:20:04 +0300

    ibb.co.js

---
+++ ibb.co.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @include        https://ibb.co/*
+// ==/UserScript==
+
+opera.addEventListener('BeforeExternalScript', function(js) {
+  // opera.postError(js.element)
+  js.preventDefault()
+}, false)
+
+opera.addEventListener('BeforeScript', function(js) {
+  // opera.postError(js.element)
+  js.preventDefault()
+}, false)
+
+!(function(global) {
+  var waitCommon = function(fn) {
+    if(global.Bga) {
+      fn()
+    }
+    else {
+      setTimeout(function() {
+        waitCommon(fn)
+      }, 0)
+    }
+  }
+  
+  waitCommon(function() {
+    with(Bga) {
+      var log = 1 ? logRaw : logNull
+      opera.postError("here")
+      onDOMReady(function() {
+        //# direct image url
+        imgUrl = null
+        document.head.getElementsByTagName("meta").each(function(meta) {
+          if(meta.getAttribute("property") == "og:image") {
+            imgUrl = meta.getAttribute("content")
+          }
+        })
+        if(imgUrl != null) {
+          location.replace(imgUrl)
+        }
+        else {
+          
+        }
+      })
+    }
+  })
+})(this)
