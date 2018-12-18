ba8b7978ef8f17f0df3d8af8dcb7894ce6a90063
bga <bga.email@gmail.com>
2018-12-18 15:45:47 +0300

    postMessage intercept + async scripts delay

--- freezeBackgroudActivity.js
+++ freezeBackgroudActivity.js
@@ -10,11 +10,121 @@
   var log = function() {
     if(0) console.log.apply(console, arguments)
   }
+  var trackActivity = function(msg) {
+    if(1) console.log.apply(console, arguments)
+  }
   
   var yes = !0, no = !1
   var CAPTURE_PHASE = yes
+  var DEEP = true
   //# assime setTimeout/setInterval ids get increments and share one counter
   //# get last id
+  
+  //# make { patchedFn } looks like { nativeFn }
+  var natify = function(patchedFn, nativeFn) {
+    var nativeCodeString = nativeFn.toString()
+    var toStringNativeCodeString = nativeFn.toString.toString()
+    patchedFn.toString = function() {
+      return nativeCodeString
+    }
+    patchedFn.toString.toString = function() {
+      return toStringNativeCodeString
+    }
+    patchedFn.toString.toString.toString = patchedFn.toString.toString
+    
+    return patchedFn
+  }
+
+  if(1) (function() {
+    var x = "".bold.toString() 
+    var y = "".bold.toString.toString() 
+    String.prototype.bold = natify(function() {
+      return "*" + this + "*"
+    }, String.prototype.bold)
+    console.assert("a".bold() == "*a*")
+    console.assert("".bold.toString() == x)
+    console.assert("".bold.toString.toString() == y)
+    console.assert("".bold.toString.toString.toString() == y)
+    console.assert("".bold.toString.toString.toString.toString() == y)
+    //# etc
+  })()
+  
+  ;if(1) (function() {
+    var log = function() {
+      if(1) console.log.apply(console, arguments)
+    }
+    
+    // opera has some issue with DOMContentLoaded
+    var onDOMReady = (function() {
+      var _isLoaded = (
+        ('readyState' in document) && function() { // stupid opera
+          return document.readyState == 'loaded' || document.readyState == 'complete'
+        }
+        || function() {
+          return document.body
+        }
+      ) 
+      var _attachEvent = (
+        ('onreadystatechange' in document) && function(_fn)
+        {
+          document.addEventListener('readystatechange', _fn, false)
+        } ||
+        function(_fn)
+        {
+          document.addEventListener('DOMContentLoaded', _fn, false);
+        }
+      )
+    
+      return function(_fn) {
+        if(_isLoaded())
+          _fn()
+        else
+          _attachEvent(_fn)
+      }
+    })()
+    
+    var pendingScripts = []
+    window.addEventListener("focus", function(e) {
+      var v = null
+      while(v = pendingScripts.shift()) {
+        log("unblock script", v.node.src)
+        /*
+        var scriptDomNode = document.createElement("script")
+        
+        scriptDomNode.src = v.src
+        scriptDomNode.async = v.async
+        scriptDomNode.defer = v.defer
+        document.head.appendChild(scriptDomNode)
+        */
+        var oldNode = v.node
+        oldNode.parentNode.insertBefore(oldNode.cloneNode(DEEP), oldNode)
+        oldNode.parentNode.removeChild(oldNode)
+      }
+    }, CAPTURE_PHASE)
+
+    onDOMReady(function() {
+      log("BeforeExternalScript")
+      opera.addEventListener('BeforeExternalScript', function(js) {
+        if(document.hidden == no) {
+          var src = js.element.src
+          log("pass script", src)
+        }
+        else if(1 || js.element.async) {
+          var src = js.element.src
+          log("block script", src)
+          trackActivity("async script", src)
+          pendingScripts.push({ node: js.element })
+          js.preventDefault()
+        }
+        else {
+          
+        }
+      }, false
+    )
+
+    })
+  })()
+
   
   var nextThreadId = (function() {
     var lastId = setTimeout("")
@@ -36,7 +146,7 @@
       }
     }, CAPTURE_PHASE)
     
-    return function() {
+    return natify(function() {
       //# tab is active => pass as is
       if(document.hidden == no) {
         log("setTimeout pass")
@@ -48,7 +158,7 @@
         queue.push({ args: Array.prototype.slice.call(arguments) })
         return nextThreadId();
       }
-    }
+    }, hookedFn)
   })()
 
   global.setInterval = (function() {
@@ -63,7 +173,7 @@
       }
     }, CAPTURE_PHASE)
     
-    return function() {
+    return natify(function() {
       //# tab is active => pass as is
       if(document.hidden == no) {
         log("setInterval pass")
@@ -75,7 +185,7 @@
         queue.push({ args: Array.prototype.slice.call(arguments) })
         return nextThreadId();
       }
-    }
+    }, hookedFn)
   })()
   
   if(global.setImmediate != null) global.setImmediate = (function() {
@@ -90,7 +200,7 @@
       }
     }, CAPTURE_PHASE)
     
-    return function() {
+    return natify(function() {
       //# tab is active => pass as is
       if(document.hidden == no) {
         log("setImmediate pass")
@@ -102,6 +212,37 @@
         queue.push({ args: Array.prototype.slice.call(arguments) })
         return nextThreadId();
       }
+    }, hookedFn)
+  })()
+  
+  if(Window.prototype.postMessage != null) Window.prototype.postMessage = (function() {
+    var log = function() {
+      if(1) console.log.apply(console, arguments)
     }
+
+    var hookedFn = Window.prototype.postMessage
+    var queue = []
+    
+    window.addEventListener("focus", function(e) {
+      var v = null
+      while(v = queue.shift()) {
+        trackActivity("postMessage unfreeze", v.args.join("\t"))
+        hookedFn.apply(v.self, v.args)
+      }
+    }, CAPTURE_PHASE)
+    
+    return natify(function() {
+      //# tab is active => pass as is
+      if(document.hidden == no) {
+        trackActivity("postMessage pass")
+        return hookedFn.apply(this, arguments)
+      }
+      // tab inactive, push to queue and execute when tab get focused again
+      else {
+        trackActivity("postMessage freeze")
+        queue.push({ self: this, args: Array.prototype.slice.call(arguments) })
+        return void 0;
+      }
+    }, hookedFn)
   })()
 })(this)
