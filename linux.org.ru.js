99889af02a85141e676668bc3ddbb4437a35478d
bga <bga.email@gmail.com>
2019-02-01 23:30:43 +0300

    if(1) + works on https:

--- linux.org.ru.js
+++ linux.org.ru.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @include        http://www.linux.org.ru/*
+// @include        https://www.linux.org.ru/*
 // @name LOR correct pagePrev/Next
 // @author Bga
 // @version 0.1
@@ -17,7 +17,7 @@ opera.addEventListener('BeforeScript', function(js) {
 }, false)
 
 
-//# annoyng [#comment]
+//# annoying [#comments] anchor.  
 if(location.hash == "#comments") {
   //# set "#" instead, jumps to top
   // location.hash = ""
@@ -38,54 +38,58 @@ if(location.hash == "#comments") {
   
   waitCommon(function() {
     with(Bga) {
-      // disable logging
+      //# disable logging
       var opera = { postError: function() {  } }
       
       var main = function(e) {
-        var prevHref = xPath("//a[@class='page-number' and position() = 1]").each(function(v) {
-          opera.postError("prev " + v.href)
-          return v.href
-        })
-        var nextHref = xPath("//a[@class='page-number' and position() = count(../a)]").each(function(v) {
-          opera.postError("next " + v.href)
-          return v.href
-        })
-
-        var prevs = xPath("//*[@rel='prev' or @rel='Prev']")
-        if(prevs.length == 0) {
-          if(prevHref != null) {
-            document.head.appendChild(de("<link rel=prev href='" + prevHref + "'/>"))
-          }
-        }
-        else {
-          prevs.each(function(v) {
-            opera.postError("del " + v.href)
-            if(v.tagName == "LINK") {
-              v.href = prevHref 
-            }
-            else {
-              v.removeAttribute("rel")
-            }
+        
+        //# Alt+Left/Right jumps by comments, not by topics
+        if(1) {
+          var prevHref = xPath("//a[@class='page-number' and position() = 1]").each(function(v) {
+            opera.postError("prev " + v.href)
+            return v.href
           })
-        }
-
-        var nexts = xPath("//*[@rel='next' or @rel='Next']")
-        if(nexts.length == 0) {
-          if(nextHref != null) {
-            document.head.appendChild(de("<link rel=next href='" + nextHref + "'/>"))
-          }
-        }
-        else {
-          nexts.each(function(v) {
-            opera.postError("del " + v.href)
-            if(v.tagName == "LINK") {
-              v.href = nextHref 
-            }
-            else {
-              v.removeAttribute("rel")
-            }
+          var nextHref = xPath("//a[@class='page-number' and position() = count(../a)]").each(function(v) {
+            opera.postError("next " + v.href)
+            return v.href
           })
-        }  
+
+          var prevs = xPath("//*[@rel='prev' or @rel='Prev']")
+          if(prevs.length == 0) {
+            if(prevHref != null) {
+              document.head.appendChild(de("<link rel=prev href='" + prevHref + "'/>"))
+            }
+          }
+          else {
+            prevs.each(function(v) {
+              opera.postError("del " + v.href)
+              if(v.tagName == "LINK") {
+                v.href = prevHref 
+              }
+              else {
+                v.removeAttribute("rel")
+              }
+            })
+          }
+
+          var nexts = xPath("//*[@rel='next' or @rel='Next']")
+          if(nexts.length == 0) {
+            if(nextHref != null) {
+              document.head.appendChild(de("<link rel=next href='" + nextHref + "'/>"))
+            }
+          }
+          else {
+            nexts.each(function(v) {
+              opera.postError("del " + v.href)
+              if(v.tagName == "LINK") {
+                v.href = nextHref 
+              }
+              else {
+                v.removeAttribute("rel")
+              }
+            })
+          }  
+        }
       }
       
       onDOMReady(main)
