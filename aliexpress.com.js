e8fb74aae1570c37d982b70753b2953202a42bbe
bga <bga.email@gmail.com>
2019-04-09 18:31:57 +0300

    Short link in english pages too. Search w/ free shipping and sort by lowest price by default

--- aliexpress.com.js
+++ aliexpress.com.js
@@ -15,6 +15,20 @@ opera.addEventListener('BeforeScript', function(js) {
 }, false)
 */
 
+if(location.pathname.match(/\/wholesale$/) || location.pathname.match(/\/w\//)) (function() {
+  var newQuery = location.search
+  //# sort by price from lowest to highest by default
+  newQuery.match(/(^|&|\?)SortType=.*?(&|$)/) || (newQuery += "&SortType=price_asc")
+  //# free shipping by default
+  newQuery.match(/(^|&|\?)isFreeShip=.*?(&|$)/) || (newQuery += "&isFreeShip=y")
+  if(newQuery != location.search) {
+    location.search = (location.search == "" ? "?" + newQuery.slice(1): newQuery)
+  }
+  else {
+    
+  }
+})()
+
 !(function(global) {
   var waitCommon = function(fn) {
     if(global.Bga) {
@@ -34,7 +48,7 @@ opera.addEventListener('BeforeScript', function(js) {
       
       onDOMReady(function() {
         //# adds "Short Url" garbage free link near "Show in english"
-        var multiLanguageSwitch = document.getElementsByClassName("multi-language-switch")[0]
+        var multiLanguageSwitch = document.getElementsByClassName("multi-language-switch")[0] || document.getElementsByClassName("product-name")[0] 
         if(multiLanguageSwitch != null) {
           var shortUrl = "".concat(location.protocol, "//", [].concat(["www"], location.host.split(".").slice(1)).join("."), location.pathname)
           multiLanguageSwitch.appendChild(de("".concat('<a href="', shortUrl, '">Short URL</a>')))
