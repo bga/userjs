36ece2a8521c9146ac6f799642734126a02a61e9
bga <bga.email@gmail.com>
2020-07-07 14:39:54 +0300

    copy of code from [youtube.js] to show WMPlayer

--- invidio.us.js
+++ invidio.us.js
@@ -8,6 +8,8 @@
 
 ;(function(undefined) {
 
+var volume = 0;
+
 opera.addEventListener('BeforeExternalScript', function(js) {
   // opera.postError(js.element)
   js.preventDefault()
@@ -55,14 +57,40 @@ opera.addEventListener('BeforeScript', function(js) {
           // var player = document.getElementById("default-language-message") || document.getElementById("player-api-legacy") || document.getElementById("player")
           var player = document.getElementById("player-container")
           playerVideo = player.getElementsByTagName("VIDEO")[0]
-          var w = 640 * 0.7;
-          var h = 480 * 0.7;
-          var volume = 0
-          var src = [].slice.call(playerVideo.getElementsByTagName("SOURCE")).map(function(v) { return v.src }).filter(function(url) { return url.match(/\&itag=18/) })[0]
-          log(src)
-          var videoDom = de("".concat('<embed src="', src, '" autostart="true" showcontrols="true" showstatusbar="1" type="application/x-mplayer2" bgcolor="white" style="width: ', w, 'px; height: ', h, 'px" volume="', volume, '">'))
+          var W = 640 * 0.7;
+          var H = 480 * 0.7;
 
-          playerVideo.replace(videoDom);
+          var showMediaPlayer = function(url, w, h) {
+            //# ui height
+            h += 72
+
+            //# player.lastChild.innerHTML = "".concat('<embed src="', url, '" autostart="true" showcontrols="true" showstatusbar="1" type="application/x-mplayer2" bgcolor="white" width="', w, 'px" height="', h, 'px" volume="', volume, '">')
+            if(0) debugger
+            playerVideo.replace(de("".concat('<embed src="', url, '" autostart="true" showcontrols="true" showstatusbar="1" type="application/x-mplayer2" bgcolor="white" style="width: ', w, 'px; height: ', h, 'px" volume="', volume, '">')))
+
+          }
+
+
+          playerVideo.getElementsByTagName("SOURCE").each(function(v) {
+            var url = v.getAttribute("src")
+            var t = v.getAttribute("label")
+            var link = de("".concat('<a href="', v.src, '" title="', t,  '">', t, '</a> '))
+
+            link.childNodes[0].onclick = function() {
+              showMediaPlayer(url, W, H)
+              return false
+            }
+
+            link.childNodes[0].setAttribute("accesskey", ({
+              'video/mp4; codecs="avc1.42001E, mp4a.40.2"': "4",
+              'video/mp4; codecs="avc1.64001F, mp4a.40.2"': "7",
+            })[v.getAttribute("type")] || null)
+
+            player.appendChild(link)
+          })
+          // var src = [].slice.call(playerVideo.getElementsByTagName("SOURCE")).map(function(v) { return v.src }).filter(function(url) { return url.match(/\&itag=22/) })[0]
+          // log(src)
+          // var videoDom = de("".concat('<embed src="', src, '" autostart="true" showcontrols="true" showstatusbar="1" type="application/x-mplayer2" bgcolor="white" style="width: ', w, 'px; height: ', h, 'px" volume="', volume, '">'))
         })()
       })
     }
