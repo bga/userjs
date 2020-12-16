3082650c3fc716adcc360c5bb2b3741e0413d089
bga <bga.email@gmail.com>
2020-12-17 01:05:30 +0300

    + `.escapeHTML`

--- common.js
+++ common.js
@@ -715,5 +715,33 @@
     }
     return t
   }
+  
+  /*! https://mths.be/he v1.1.1 by @mathias | MIT license */
+  Bga.escapeHTML = (function() {
+    var regexEscape = /["&'<>`]/g;
+    var escapeMap = {
+      '"': '&quot;',
+      '&': '&amp;',
+      '\'': '&#x27;',
+      '<': '&lt;',
+      // See https://mathiasbynens.be/notes/ambiguous-ampersands: in HTML, the
+      // following is not strictly necessary unless it’s part of a tag or an
+      // unquoted attribute value. We’re only escaping it to support those
+      // situations, and for XML support.
+      '>': '&gt;',
+      // In Internet Explorer ≤ 8, the backtick character can be used
+      // to break out of (un)quoted attribute values or HTML comments.
+      // See http://html5sec.org/#102, http://html5sec.org/#108, and
+      // http://html5sec.org/#133.
+      '`': '&#x60;'
+    };
+    
+    return function(string) {
+      return string.replace(regexEscape, function($0) {
+        // Note: there is no need to check `has(escapeMap, $0)` here.
+        return escapeMap[$0];
+      });
+    }
+  })()
 
 })(this)
